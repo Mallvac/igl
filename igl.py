5d80cb228b79d491e6e1ae89429cf426c1304ec4
Mallvac <74984378+Mallvac@users.noreply.github.com>
2022-03-20 17:54:33 +0100

    code furrify
    OwO

--- igl.py
+++ igl.py
@@ -8,8 +8,8 @@ driver = webdriver.Chrome("chromedriver.exe")
 
 driver.get("https://www.instagram.com/p/CbSeHMaOaTP/")
 driver.find_element(By.XPATH, "/html/body/div[4]/div/div/button[1]").click()
-print("As of time I can't get my app authorised, so you gotta login into your facebook account. Sorry :(")
-email = input("User Email:")
-password = input("User Password:")
+print("As of twime I can't gwet my app authowised, so you gotta login into youw facebook accowount. Sowwy :(")
+email = input("Usew Email:")
+password = input("Usew Passwowd:")
 
 time.sleep(100)
